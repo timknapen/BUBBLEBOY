0375bcd3c2117eb146ed9b81fa0f0674a3d259c1
Tim <timknapen@gmail.com>
2019-10-18 16:06:12 +0200

    Added working LIDAR test with TFMini

--- LIDAR_test/LIDAR_test.ino
+++ LIDAR_test/LIDAR_test.ino
@@ -11,36 +11,22 @@
 ***********************************************************/
 
 /*
-Example code for Benewake TFMini time-of-flight distance sensor. 
-by Peter Jansen (December 11/2017)
+Based on example code for Benewake TFMini time-of-flight distance 
+sensor by Peter Jansen (December 11/2017)
 This example code is in the public domain.
-
-This example communicates to the TFMini using a SoftwareSerial port at 115200, 
-while communicating the distance results through the default Arduino hardware
-Serial debug port. 
-
-SoftwareSerial for some boards can be unreliable at high speeds (such as 115200). 
-The driver includes some limited error detection and automatic retries, that
-means it can generally work with SoftwareSerial on (for example) an UNO without
-the end-user noticing many communications glitches, as long as a constant refresh
-rate is not required. 
-
-The (UNO) circuit:
- * Uno RX is digital pin 10 (connect to TX of TF Mini)
- * Uno TX is digital pin 11 (connect to RX of TF Mini)
- 
 */
+
+#define TXPIN 26 // white wire on TFMini RX
+#define RXPIN 27 // Green wire on TFMini TX
 
 #include "TFMini.h"
 
-// Setup software serial port
-//SoftwareSerial mySerial(10, 11);      // Uno RX (TFMINI TX), Uno TX (TFMINI RX)
 TFMini tfmini;
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  // wait for serial port to connect. Needed for native USB port only
+
   while (!Serial)
   {
     for (int i = 0; i < 3; i++)
@@ -54,33 +40,30 @@ void setup()
   }
   // Step 1: Initialize hardware serial port (serial debug port)
   Serial.begin(115200);
-  Serial.println("Initializing...");
 
-  // Step 2: Initialize the data rate for the SoftwareSerial port
-  Serial1.begin(115200);
+  // Step 2: Initialize the data rate for the LIDAR serial port
+  Serial1.setTX(TXPIN);
+  Serial1.setRX(RXPIN);
+  Serial1.begin(TFMINI_BAUDRATE);
 
-  Serial.println("setup Serial1");
   // Step 3: Initialize the TF Mini sensor
   tfmini.begin(&Serial1);
 
-  Serial.println("Setup tfmini");
+  Serial.println("Done settting up TFmini LIDAR");
 }
 
 void loop()
 {
-  Serial.print("loop ");
-  Serial.println(millis());
-  // Take one TF Mini distance measurement
-  uint16_t dist = tfmini.getDistance();
-  Serial.println("Received distance");
+  
 
+  uint16_t dist = tfmini.getDistance();
   uint16_t strength = tfmini.getRecentSignalStrength();
-  Serial.println("Received Signal strength");
 
   // Display the measurement
   Serial.print(dist);
-  Serial.print(" cm      sigstr: ");
-  Serial.println(strength);
+  //Serial.print(" ");
+  Serial.print(strength);
+  Serial.println();
 
   // Wait some short time before taking the next measurement
   delay(25);
